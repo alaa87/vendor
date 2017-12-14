f8c04612b57b3c4be8d80fba8775819c08e5baf7
a.abukhalil <a.abukhalil@certacure.com>
2017-12-14 16:26:07 +0200

    Initial commit

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -8,16 +8,6 @@ import { Routes } from '@angular/router';
     <ven-sidebar></ven-sidebar>
     <router-outlet></router-outlet>
     `
-
-    // <ba-sidebar></ba-sidebar>
-    // <ba-page-top></ba-page-top>
-    // <div class="al-main">
-    //   <div class="al-content">
-    //     <!--<ba-content-top></ba-content-top>-->
-    //     <router-outlet></router-outlet>
-    //   </div>
-    // </div>
-    // <ba-footer></ba-footer>
 })
 export class Pages {
 
